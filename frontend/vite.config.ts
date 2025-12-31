ba9f3f529f873d59c3f52192da12c14c28bac91e
Nacereddine <ngaceb@yahoo.fr>
2026-01-01 00:40:55 +0100

    fixing render 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://medical-xlkp.onrender.com',
         changeOrigin: true,
       },
     },
